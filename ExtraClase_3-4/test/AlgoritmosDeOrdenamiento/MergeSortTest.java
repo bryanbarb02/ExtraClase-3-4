39f954c7f7046569e5d26b45bf15d6b37f3a47c1
bryanbarb02 <37256420+bryanbarb02@users.noreply.github.com>
2019-05-24 16:10:25 -0600

    Update MergeSortTest.java

--- ExtraClase_3-4/test/AlgoritmosDeOrdenamiento/MergeSortTest.java
+++ ExtraClase_3-4/test/AlgoritmosDeOrdenamiento/MergeSortTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AlgoritmosDeOrdenamiento;
 
 import Lista.LinkedList;
@@ -19,6 +15,80 @@ import static org.junit.Assert.*;
  * @author Bryan
  */
 public class MergeSortTest {
+        
+    /**
+     * testInsertion1 Comprueba que la lista esta vacía
+     */
+    @Test
+    public void testMerge1() {
+        System.out.println("testInsertion1");
+        LinkedList merge = new LinkedList();
+        assertEquals(0 , merge.tamano());
+        MergeSort.Merge(merge);
+
+    }
+    
+    /**
+     * testInsertion2() Comprueba la cantidad de elementos de tiene la lista
+     */
+    @Test
+    public void testMerge2() {
+        System.out.println("testInsertion2");
+        LinkedList merge = new LinkedList();
+        merge.add(5);
+        merge.add(4);
+        merge.add(9);
+        merge.add(1);
+        merge.add(15);
+        merge.add(2);
+        merge.add(8);
+        assertEquals(7, merge.tamano());    
+        MergeSort.Merge(merge);
+        assertEquals(7, merge.tamano());
+    }
+    
+    /**
+     * testInsertion3() Inserta un nuevo valor en el íncice seleccionado
+     */
+    @Test
+    public void testMerge3() {
+        System.out.println("testInsertion3");
+        LinkedList merge = new LinkedList();
+        merge.add(5);       
+        merge.set(0, 99);   
+        assertEquals(99, merge.get(0));
+        MergeSort.Merge(merge);
+        assertEquals(99, merge.get(0));
+    }
+    
+    /**
+     * testInsertion4() Remueve un nodo según el indice seleccionado
+     */
+    @Test
+    public void testMerge4() {
+        System.out.println("testInsertion4");
+        LinkedList merge = new LinkedList();
+        merge.add(1);
+        merge.remover(0);
+        assertEquals(0 , merge.tamano());
+        MergeSort.Merge(merge);
+    }
+    
+    /**
+     * testInsertion5() Comprueba que se ordenó la lista correctamente
+     */
+    @Test
+    public void testMerge5() {
+        System.out.println("testInsertion5");
+        LinkedList merge = new LinkedList();
+        merge.add(2);
+        merge.add(3);
+        merge.add(1);
+        MergeSort.Merge(merge);
+        assertEquals(1, merge.get(0));
+        assertEquals(2, merge.get(1));
+        assertEquals(3, merge.get(2));  
+    }
     
     public MergeSortTest() {
     }
@@ -37,18 +107,6 @@ public class MergeSortTest {
     
     @After
     public void tearDown() {
-    }
-
-    /**
-     * Test of Merge method, of class MergeSort.
-     */
-    @Test
-    public void testMerge() {
-        System.out.println("Merge");
-        LinkedList lista = null;
-        MergeSort.Merge(lista);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
