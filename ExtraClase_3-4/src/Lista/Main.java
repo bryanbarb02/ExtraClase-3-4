1391de6624ef1c08a657e84369792503cb45f3fe
bryanbarb02 <37256420+bryanbarb02@users.noreply.github.com>
2019-05-24 23:09:44 -0600

    Update Main.java

--- ExtraClase_3-4/src/Lista/Main.java
+++ ExtraClase_3-4/src/Lista/Main.java
@@ -1,10 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Lista;
-//import AlgoritmosDeOrdenamiento.*;
+
+import AlgoritmosDeOrdenamiento.RadixSort;
+import Lista.InsertionSort;
+import Lista.LinkedList;
+import Lista.MergeSort;
+//import Lista.QuickSort;
+import Lista.QuickSort_1;
 
 /**
  *
@@ -12,53 +12,68 @@ package Lista;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        LinkedList l = new LinkedList();
-        l.add(5);
-        l.add(4);
-        l.add(9);
-        l.add(1);
         
-//        System.out.println(l.get(0));
-//        System.out.println(l.get(1));
-//        System.out.println(l.get(2));
-//        System.out.println(l.get(3));
-//        System.out.println("");
+        System.out.println("InsertionSort: ");
+        LinkedList insertion = new LinkedList();
+        insertion.add(5);
+        insertion.add(4);
+        insertion.add(9);
+        insertion.add(1);
+        insertion.add(15);
+        insertion.add(2);
+        insertion.add(8);
+        insertion.set(0, 99);
+        
+        System.out.println(insertion);
+        InsertionSort.Insertion(insertion);
+        System.out.println(insertion + "\n");
+        
+        System.out.println("MergeSort");
+        LinkedList merge = new LinkedList();
+        merge.add(6);
+        merge.add(9);
+        merge.add(4);
+        merge.add(1);
+        merge.add(5);
+        merge.add(2);
+        merge.add(8);
+        
+        System.out.println(merge);
+        MergeSort.Merge(merge);        
+        System.out.println(merge + "\n");
+        
+        
+        System.out.println("QuickSort");
+        LinkedList quick = new LinkedList();
+        quick.add(0);
+        quick.add(45);
+        quick.add(65);
+        quick.add(3);
+        quick.add(76);
+        quick.add(23);
+        quick.add(87);
+        quick.add(82);
+        
+        
+        
+        
+        System.out.println(quick);
+        System.out.println(quick + "\n");
+        
+//        System.out.println("RadixSort");
+//        LinkedList radix = new LinkedList();
+//        radix.add(45);
+//        radix.add(65);
+//        radix.add(23);
+//        radix.add(76);
+//        radix.add(23);
+//        radix.add(87);
+//        radix.add(82);
 //        
-//        l.swap(1, 2);
-        
-        System.out.println(l.get(0));
-        System.out.println(l.get(1));
-        System.out.println(l.get(2));
-        System.out.println(l.get(3));
-        System.out.println("");
-        System.out.println("Insertion");
-//        InsertionSort.Insertion(l);
-//        System.out.println("Insertion");
-//        System.out.println(l.get(0));
-//        System.out.println(l.get(1));
-//        System.out.println(l.get(2));
-//        System.out.println(l.get(3));
-//        System.out.println("");
-        
-//          MergeSort.Merge(l);
-        System.out.println("Quik");
-        QuickSort.Quick(l);
-        
-        System.out.println(l.get(0));
-        System.out.println(l.get(1));
-        System.out.println(l.get(2));
-        System.out.println(l.get(3));
-        System.out.println("");
-        
-//        System.out.println(l.get(0));
-//        System.out.println(l.get(1));
-//        System.out.println(l.get(2));
-//        System.out.println(l.get(3));
-        
+//        System.out.println(radix);
+////        RadixSort.radix();
+//        System.out.println(radix + "\n");
         
     }
     
